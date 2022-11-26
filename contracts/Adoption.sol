e68efe7a083715d4975472ed5e42b0ad42797220
sakaguchi <runrunrun4649@icloud.com>
2022-11-26 18:59:51 +0900

    1126

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 contract Adoption {
 address[16] public adopters;
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
+//App というJSON を記述しています
 App = {
   web3Provider: null,
   contracts: {},
 
+  //このJSON メソッドではpets.json のデータをHTML に送ります
   init: async function() {
-    // Load pets.
+    // index.html の54行目でインポートしているJQuery を使って16匹のペットの情報を持ってるpets.json からJSON を取得し変数data に代入しています
     $.getJSON('../pets.json', function(data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
@@ -23,63 +25,87 @@ App = {
     return await App.initWeb3();
   },
 
+  //このJSON メソッドでは、非同期処理を使ってWeb 3.js からインスタンスを初期化します
   initWeb3: async function() {
-    if(typeof web3 !== 'undefined'){
-      App.web3Provider = web3.currentProvider;
-    }else{
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    }
-
-    web3 = new Web3(App.web3Provider);
+      //もし、イーサリアムノードにweb 3インスタンスすでにあれば、そっちを使う
+      if (typeof web3 !== 'undefined') {
+          　App.web3Provider = web3.currentProvider;
+      } else {
+          　//localhost:9545 にあるコントラクトを呼び出す
+          　App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      }
+      //3行目にあるweb3Provider を呼び出しWeb 3インスタンスを生成する
+      web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
+  //コントラクトをJavaScript で扱えるようをインスタンス化します
   initContract: function() {
-    $.getJSON('Adoption.json',function(data){
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffContract(AdoptionArtifact);
-
-      App.contracts.Adoption.setProvider(App.web3Provider);
-
-      return App.markAdopted();
-    });
-
+      $.getJSON('Adoption.json', function(data) {
+          　console.log("Adoption.json は、" + JSON.stringify(data));
+          　var AdoptionArtifact = data;
+          　//truffle-contract.js (ライブラリ)を使いコントラクトをJavaScript で扱えるように変換
+          　App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+          　console.log('App.contracts.Adoption は、' + App.contracts.Adoption)
+            //35行目で呼び出したコントラクトをプロパイダー(Metamask)に投げます。
+          　App.contracts.Adoption.setProvider(App.web3Provider);
+          　//63行目から始まるmarkAdopted を実行
+          　return App.markAdopted();
+      });
+    //59行目から始めるbindEvents を実行
     return App.bindEvents();
   },
 
   bindEvents: function() {
+    //Adopt ボタンを押すと86行目のhandleAdopt のJSON メソッドを実行
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  markAdopted: function(adopters, account) {
-    var adoptionInstance;
-    App.contracts.Adoption.deployed().then(function(instance){
-      adoptionInstance = instance;
-
-      return adoptionInstance.getAdopters.call();
-    }).then(function(adopters){
-
-      /*adoptersを順番に取り出し「0じゃない　＝既に引き取られているものがあればパネルのボタンの文字をSuccessにし、非表示にする*/
-      for(i=0; i<adopters.length; i++){
-        if(adopters[i] !== "0x0000000000000000000000000000000000000000"){
-          $(".panel-pet").eq(i).find("button").text("Success").attr('disabled', true);
-        }
-      }
-      
-    }).catch(function(err){
-      console.log(err.message);
-    });
+  //ペットたちの購入状況を取得し、購入済みだったらボタンをSuccess に変更する
+  markAdopted: function() {
+      var adoptionInstance;
+      App.contracts.Adoption.deployed().then(function(instance) {
+          　adoptionInstance = instance;
+            //call メソッドを使用してコントラクトから変数を取り出すことができgas 代をかけずに取り出すことができる
+          　return adoptionInstance.getAdopters.call();
+      }).then(function(adopters) {
+          　for (i = 0; i < adopters.length; i++) {
+                 //もしすでにこのペットが購入済みだったら
+              　　if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+                    　　//JQuery を使ってSuccess と表示させる
+                  　　　$('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+              　　}
+          　}
+      }).catch(function(err) {
+          　console.log(err.message);
+      });
   },
 
+  //
   handleAdopt: function(event) {
+    console.dir(event);
+    //JavaScript のpreventDefault メソッドを使って の処理を禁止します
     event.preventDefault();
 
+    //JQuery を使ってindex.html の47行目のdata-id 属性の文字列を取得し整数に変換しpetId に代入
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+    web3.eth.getAccounts(function(error, accounts) {
+        　if (error) {
+            　　console.log(error);
+        　}
+        　var account = accounts[0];
+        　App.contracts.Adoption.deployed().then(function(instance) {
+            　　adoptionInstance = instance;
+            　　return adoptionInstance.adopt(petId, {from: account});
+        　}).then(function(result) {
+            　　return App.markAdopted();
+        　}).catch(function(err) {
+            　　console.log(err.message);
+        　});
+    });
   }
 
 };
